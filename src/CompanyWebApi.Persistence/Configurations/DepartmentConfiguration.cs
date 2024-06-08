2cfec5767c2eba0218cef7c659cd23f393959e56
Jon <31523807+jsgitit@users.noreply.github.com>
2024-06-08 10:29:34 -0700

    cleaned up using

--- src/CompanyWebApi.Persistence/Configurations/DepartmentConfiguration.cs
+++ src/CompanyWebApi.Persistence/Configurations/DepartmentConfiguration.cs
@@ -4,26 +4,26 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace CompanyWebApi.Persistence.Configurations
 {
-	public class DepartmentConfiguration
-	{
-		public DepartmentConfiguration(EntityTypeBuilder<Department> entity)
-		{
-			// Table
-			entity.ToTable("Departments");
+    public class DepartmentConfiguration
+    {
+        public DepartmentConfiguration(EntityTypeBuilder<Department> entity)
+        {
+            // Table
+            entity.ToTable("Departments");
 
             // Keys
-			entity.HasKey(dep => dep.DepartmentId);
+            entity.HasKey(dep => dep.DepartmentId);
 
             // Properties
-			entity.Property(dep => dep.Name)
-				.IsRequired();
+            entity.Property(dep => dep.Name)
+                .IsRequired();
 
-			// Relationships
-			entity.HasMany(dep => dep.Employees)
-				.WithOne(emp => emp.Department)
+            // Relationships
+            entity.HasMany(dep => dep.Employees)
+                .WithOne(emp => emp.Department)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-				.HasForeignKey(emp => emp.DepartmentId)
-				.OnDelete(DeleteBehavior.Cascade);
-		}
-	}
+                .HasForeignKey(emp => emp.DepartmentId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
 }
--- src/CompanyWebApi.Persistence/Configurations/UserConfiguration.cs
+++ src/CompanyWebApi.Persistence/Configurations/UserConfiguration.cs
@@ -4,32 +4,32 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace CompanyWebApi.Persistence.Configurations
 {
-	public class UserConfiguration
-	{
-		public UserConfiguration(EntityTypeBuilder<User> entity)
-		{
-			// Table
-			entity.ToTable("Users");
-			
-			// Keys 
-			entity.HasKey(e => e.EmployeeId);
+    public class UserConfiguration
+    {
+        public UserConfiguration(EntityTypeBuilder<User> entity)
+        {
+            // Table
+            entity.ToTable("Users");
 
-			// Properties
-			entity.Property(e => e.Username)
-				.IsRequired();
+            // Keys 
+            entity.HasKey(e => e.EmployeeId);
 
-			entity.Property(e => e.Password)
-				.IsRequired();
+            // Properties
+            entity.Property(e => e.Username)
+                .IsRequired();
+
+            entity.Property(e => e.Password)
+                .IsRequired();
 
             // Indexes
             entity.HasIndex(b => b.Username)
                 .IsUnique();
 
-			// Relationships
-			entity.HasOne(a => a.Employee)
-				.WithOne(b => b.User)
-				.HasForeignKey<User>("EmployeeId")
-				.OnDelete(DeleteBehavior.Cascade);
-		}
-	}
+            // Relationships
+            entity.HasOne(a => a.Employee)
+                .WithOne(b => b.User)
+                .HasForeignKey<User>("EmployeeId")
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
 }
