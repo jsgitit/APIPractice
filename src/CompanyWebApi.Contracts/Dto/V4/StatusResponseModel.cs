128e46149e836d24558c8f0da9c68bb3d644d305
Jon <31523807+jsgitit@users.noreply.github.com>
2024-08-27 10:21:19 -0700

    StatusController.cs - updated method from Get to GetStatus - converted to file scoped namespace - copied the StatusModel.cs to the \Dto\StatusResponseModel.cs for future versioning.  Will remove StatusResponse.cs after V3 is removed - revised to use the StatusReponseModel
    StatusResponseModel.cs
    - changed for file scoped namespace, and new namespace

---
+++ src/CompanyWebApi.Contracts/Dto/V4/StatusResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CompanyWebApi.Contracts.Dto.V4;
+
+public class StatusResponseModel
+{
+    public string AssemblyName { get; set; }
+
+    public string AssemblyVersion { get; set; }
+
+    public string StartTime { get; set; }
+
+    public string Host { get; set; }
+}
